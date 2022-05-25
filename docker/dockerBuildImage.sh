c9424570494e9b421011adeb169b683db6a1fe48
meglaficus <jakobmeglic123@gmail.com>
2022-05-25 15:37:08 +0200

    initial

---
+++ docker/dockerBuildImage.sh
@@ -0,0 +1,1 @@
+docker build -t nnunet:0.1 --build-arg env_det_num_threads=6 --build-arg env_det_verbose=1 .
---
+++ docker/dockerSpawnContainer.sh
@@ -0,0 +1,2 @@
+docker run --gpus all -it --shm-size=24gb nnunet:0.1
+# -v /home/coline/ObjectDetector:/ObjectDetector

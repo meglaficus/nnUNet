17ec435d8c0cec7892b3faaf021dfe7756e6b97b
meglaficus <jakobmeglic123@gmail.com>
2022-05-26 13:35:24 +0200

    sdf

--- docker/dockerSpawnContainer.sh
+++ docker/dockerSpawnContainer.sh
@@ -1,5 +1,5 @@
 
-docker run --gpus all -it -v C:\Users\jakob\Documents\GitHub\nnUnet\nnUNet_files:/nnUNet_files nnunet:0.1
+docker run --gpus all -it -v C:\Users\jakob\Documents\GitHub\nnUnet\nnUNet_files:/nnUNet_files --shm-size=24gb nnunet:0.2
 
 #used to spawn original test container
 #docker run --gpus all -it --shm-size=24gb nnunet:0.1

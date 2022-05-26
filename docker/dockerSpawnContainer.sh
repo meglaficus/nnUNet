9ada97c0a1e17dbce5b44f459857175abc0483a1
meglaficus <jakobmeglic123@gmail.com>
2022-05-26 12:09:26 +0200

    auto sets paths

--- docker/dockerSpawnContainer.sh
+++ docker/dockerSpawnContainer.sh
@@ -1,2 +1,8 @@
-docker run --gpus all -it --shm-size=24gb nnunet:0.1
+
+docker run --gpus all -it -v C:\Users\jakob\Documents\GitHub\nnUnet\nnUNet_files:/nnUNet_files nnunet:0.1
+
+#used to spawn original test container
+#docker run --gpus all -it --shm-size=24gb nnunet:0.1
+
+
 # -v /home/coline/ObjectDetector:/ObjectDetector
